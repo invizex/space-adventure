535e73dd06ec14598e7e77aeef33a6a224814371
Maksim Vaysbekker <92627767+invizex@users.noreply.github.com>
2022-01-02 20:11:12 +0800

    add files

---
+++ src/Game.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Threading;
+
+namespace SpaceAdventure
+{
+    class Game
+    {
+        Display display { get; set; }
+        Player player { get; set; }
+        int Speed { get; set; } // Скорость обновления экрана
+        bool isGameAvalible { get; set; } // Запущена ли игра
+
+        public Game(int speed, int chance)
+        {
+            isGameAvalible = true;
+            Speed = 10;
+            display = new Display(speed, chance);
+            player = new Player(display.Height);
+        }
+
+        public void Start() 
+        {
+            Console.Clear();
+
+            display.CreateMap();      
+
+            Thread move = new Thread(new ThreadStart(Move));
+            move.Start();
+
+            Thread update = new Thread(new ThreadStart(display.UpdateMap));
+            update.Start();
+
+            // Главный игровой цикл
+            while (isGameAvalible)
+            {
+                display.PrintMap(player.X, player.Y);
+                player.PrintInfo(display.Height);
+
+                player.Score++;
+                display.Score = player.Score;
+              
+                isCollision();
+
+                Thread.Sleep(Speed);
+            }
+
+            update.Join(0);
+            move.Join(0);
+        }
+
+        // Проверка на столкновение игрока с объектом
+        void isCollision()
+        {
+            if (display.map[player.Y][player.X] == display.MeteorSymbol)
+            {
+                if (player.Health == 1) isGameAvalible = false;
+                else player.Health--;
+            }
+
+            else if (display.map[player.Y][player.X] == display.MoneySymbol)
+            {
+                player.Money++;
+            } 
+                
+            else if (display.map[player.Y][player.X] == display.HealthSymbol && 
+                     player.Health != player.MaxHealth)
+            {
+                player.Health++;
+            }
+        }
+
+        // Вывод информации при проигрыше
+        void GameOver()
+        {
+            Console.Clear();
+
+            SlowTextPrint("►◄►◄█ GAME OVER █►◄►◄\n\n");
+            SlowTextPrint($"Счёт:      {player.Score}\n");
+            SlowTextPrint($"Деньги:    {player.Money}");
+
+            Console.CursorVisible = true;
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+
+        // Плавный вывод текста 
+        void SlowTextPrint (string text)
+        { 
+            for (int i = 0; i < text.Length; i++)
+            {
+                Thread.Sleep(80);
+                Console.Write(text[i]);
+            }
+        }
+
+        // Передвижение игрока (только по оси X)
+        void Move()
+        {
+            while (isGameAvalible)
+            {
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        player.X--;
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        player.X++;
+                        break;
+
+                    case ConsoleKey.Q:
+                        isGameAvalible = false;
+                        break;
+                }
+
+                if (player.X < 0) player.X++;
+                else if (player.X == display.Width) player.X--; 
+            }
+        }
+    }
+}
---
+++ src/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SpaceAdventure
+{
+    class Player
+    {
+        public int X { get; set; } // Позиция игрока по оси X
+        public int Y { get; } // Позиция игрока по оси Y
+        public int Health { get; set; } // Здровье
+        public int Money { get; set; } // Деньги
+        public int MaxHealth { get; set; } // Максимальное здоровье
+        public int Score { get; set; } // Игровой счёт
+
+        public Player(int Height)
+        {
+            X = 0;
+            Y = Height - 5;
+
+            Health = 5;
+            MaxHealth = 10;
+            Money = 0;
+            Score = 0;
+        }
+
+        // Вывод информации о игроке
+        public void PrintInfo(int Height)
+        {
+            Console.SetCursorPosition(0, Height + 1);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"Здоровье:    {Health}/{MaxHealth}\t");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write($"Деньги:    {Money}\t");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write($"Счёт:    {Score}");
+
+            Console.ResetColor();
+        }
+    }
+}
